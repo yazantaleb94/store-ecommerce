390bf2d94c6ef28db7b86b85502d46335473c560
yazantaleb94 <yazantaleb94@gmail.com>
2020-09-04 17:21:10 +0300

    test

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,150 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8" />
+    <title>Login | Adminox - Responsive Bootstrap 4 Admin Dashboard</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta content="A fully featured admin theme which can be used to build CRM, CMS, etc." name="description" />
+    <meta content="Coderthemes" name="author" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <!-- App favicon -->
+    <link rel="shortcut icon" href="assets/images/favicon.ico">
+
+    <!-- App css -->
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet" type="text/css" id="bootstrap-stylesheet" />
+    <link href="assets/css/icons.min.css" rel="stylesheet" type="text/css" />
+    <link href="assets/css/app.min.css" rel="stylesheet" type="text/css" id="app-stylesheet" />
+
+</head>
+
+<body class="authentication-bg bg-primary authentication-bg-pattern d-flex align-items-center pb-0 vh-100">
+
+    <div class="home-btn d-none d-sm-block">
+        <a href="index.html"><i class="fas fa-home h2 text-white"></i></a>
+    </div>
+
+    <div class="account-pages w-100 mt-5 mb-5">
+        <div class="container">
+
+            <div class="row justify-content-center">
+                <div class="col-md-8 col-lg-6 col-xl-5">
+                    <div class="card mb-0">
+
+                        <div class="card-body p-4">
+
+                            <div class="account-box">
+                                <div class="account-logo-box">
+                                    <div class="text-center">
+                                        <a href="index.html">
+                                            <img src="assets/images/logo-dark.png" alt="" height="30">
+                                        </a>
+                                    </div>
+                                    <h5 class="text-uppercase mb-1 mt-4">Sign In</h5>
+                                    <p class="mb-0">Login to your Admin account</p>
+                                </div>
+
+                                <div class="account-content mt-4">
+
+                                    <form class="form-horizontal" method="POST" action="{{ route('login') }}">
+                                        @csrf
+
+                                        <div class="form-group row">
+                                            <div class="col-12">
+                                                <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+                                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+                                                <!-- <input class="form-control" type="email" id="emailaddress" required="" placeholder="john@deo.com"> -->
+                                                @error('email')
+                                                <span class="invalid-feedback" role="alert">
+                                                    <strong>{{ $message }}</strong>
+                                                </span>
+                                                @enderror
+                                            </div>
+                                        </div>
+
+                                        <div class="form-group row">
+                                            <div class="col-12">
+                                                <a href="page-recoverpw.html" class="text-muted float-right"><small>Forgot your password?</small></a>
+                                                <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
+
+                                                <!-- <label for="password">Password</label> -->
+                                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+
+                                                <!-- <input class="form-control" type="password" required="" id="password" placeholder="Enter your password"> -->
+                                                @error('password')
+                                                <span class="invalid-feedback" role="alert">
+                                                    <strong>{{ $message }}</strong>
+                                                </span>
+                                                @enderror
+                                            </div>
+                                        </div>
+
+                                        <div class="form-group row">
+                                            <div class="col-12">
+
+                                                <div class="checkbox checkbox-success">
+                                                    <!-- <input id="remember" type="checkbox" checked=""> -->
+                                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+                                                    <!-- <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}> -->
+                                                    <label for="remember">
+                                                        Remember me
+                                                    </label>
+                                                </div>
+
+                                            </div>
+                                        </div>
+
+                                        <div class="form-group row text-center mt-2">
+                                            <div class="col-12">
+                                                <button class="btn btn-md btn-block btn-primary waves-effect waves-light" type="submit">Sign In</button>
+                                            </div>
+                                        </div>
+
+                                    </form>
+
+                                    <div class="row">
+                                        <div class="col-sm-12">
+                                            <div class="text-center">
+                                                <button type="button" class="btn mr-1 btn-facebook waves-effect waves-light">
+                                                    <i class="fab fa-facebook-f"></i>
+                                                </button>
+                                                <button type="button" class="btn mr-1 btn-googleplus waves-effect waves-light">
+                                                    <i class="fab fa-google"></i>
+                                                </button>
+                                                <button type="button" class="btn mr-1 btn-twitter waves-effect waves-light">
+                                                    <i class="fab fa-twitter"></i>
+                                                </button>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <div class="row mt-4 pt-2">
+                                        <div class="col-sm-12 text-center">
+                                            <p class="text-muted mb-0">Don't have an account? <a href="page-register.html" class="text-dark ml-1"><b>Sign Up</b></a></p>
+                                        </div>
+                                    </div>
+
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+                    <!-- end card-body -->
+                </div>
+                <!-- end card -->
+            </div>
+            <!-- end row -->
+        </div>
+        <!-- end container -->
+    </div>
+    <!-- end page -->
+
+    <!-- Vendor js -->
+    <script src="assets/js/vendor.min.js"></script>
+
+    <!-- App js -->
+    <script src="assets/js/app.min.js"></script>
+
+</body>
+
+</html>
---
+++ resources/views/auth/register.blade.php
@@ -0,0 +1,140 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8" />
+    <title>Register | Adminox - Responsive Bootstrap 4 Admin Dashboard</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta content="A fully featured admin theme which can be used to build CRM, CMS, etc." name="description" />
+    <meta content="Coderthemes" name="author" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <!-- App favicon -->
+    <link rel="shortcut icon" href="assets/images/favicon.ico">
+
+    <!-- App css -->
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet" type="text/css" id="bootstrap-stylesheet" />
+    <link href="assets/css/icons.min.css" rel="stylesheet" type="text/css" />
+    <link href="assets/css/app.min.css" rel="stylesheet" type="text/css" id="app-stylesheet" />
+
+</head>
+
+<body class="authentication-bg bg-primary authentication-bg-pattern d-flex align-items-center pb-0 vh-100">
+
+    <div class="home-btn d-none d-sm-block">
+        <a href="index.html"><i class="fas fa-home h2 text-white"></i></a>
+    </div>
+
+    <div class="account-pages w-100 mt-5 mb-5">
+        <div class="container">
+
+            <div class="row justify-content-center">
+                <div class="col-md-8 col-lg-6 col-xl-5">
+                    <div class="card mb-0">
+
+                        <div class="card-body p-4">
+
+                            <div class="account-box">
+                                <div class="account-logo-box">
+                                    <div class="text-center">
+                                        <a href="index.html">
+                                            <img src="assets/images/logo-dark.png" alt="" height="30">
+                                        </a>
+                                    </div>
+                                    <h5 class="text-uppercase mb-1 mt-4">Register</h5>
+                                    <p class="mb-0">Get access to our admin panel</p>
+                                </div>
+
+                                <div class="account-content mt-4">
+                                    <!-- <form class="form-horizontal" action="#"> -->
+                                    <form method="POST" class="form-horizontal" action="{{ route('register') }}">
+                                        @csrf
+                                        <div class="form-group row">
+                                            <div class="col-12">
+                                                <!-- <label for="username">Full Name</label> -->
+                                                <input class="form-control" type="email" id="username" required="" placeholder="Michael Zenaty">
+                                            </div>
+                                        </div>
+
+                                        <div class="form-group row">
+                                            <div class="col-12">
+                                                <label for="emailaddress">Email address</label>
+                                                <input class="form-control" type="email" id="emailaddress" required="" placeholder="john@deo.com">
+                                            </div>
+                                        </div>
+
+                                        <div class="form-group row">
+                                            <div class="col-12">
+                                                <a href="page-recoverpw.html" class="text-muted float-right"><small>Forgot your password?</small></a>
+                                                <label for="password">Password</label>
+                                                <input class="form-control" type="password" required="" id="password" placeholder="Enter your password">
+                                            </div>
+                                        </div>
+
+
+                                        <div class="form-group row">
+                                            <div class="col-12">
+
+                                                <div class="checkbox checkbox-success">
+                                                    <input id="remember" type="checkbox" checked="">
+                                                    <label for="remember">
+                                                        I accept <a href="#">Terms and Conditions</a>
+                                                    </label>
+                                                </div>
+
+                                            </div>
+                                        </div>
+
+                                        <div class="form-group row text-center mt-2">
+                                            <div class="col-12">
+                                                <button class="btn btn-md btn-block btn-primary waves-effect waves-light" type="submit">Sign Up Free</button>
+                                            </div>
+                                        </div>
+
+                                    </form>
+
+                                    <div class="row">
+                                        <div class="col-sm-12">
+                                            <div class="text-center">
+                                                <button type="button" class="btn mr-1 btn-facebook waves-effect waves-light">
+                                                    <i class="fab fa-facebook-f"></i>
+                                                </button>
+                                                <button type="button" class="btn mr-1 btn-googleplus waves-effect waves-light">
+                                                    <i class="fab fa-google"></i>
+                                                </button>
+                                                <button type="button" class="btn mr-1 btn-twitter waves-effect waves-light">
+                                                    <i class="fab fa-twitter"></i>
+                                                </button>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <div class="row mt-4 pt-2">
+                                        <div class="col-sm-12 text-center">
+                                            <p class="text-muted">Already have an account? <a href="page-login.html" class="text-dark ml-1"><b>Sign In</b></a></p>
+                                        </div>
+                                    </div>
+
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+                    <!-- end card-body -->
+                </div>
+                <!-- end card -->
+            </div>
+            <!-- end row -->
+        </div>
+        <!-- end container -->
+    </div>
+    <!-- end page -->
+
+    <!-- Vendor js -->
+    <script src="assets/js/vendor.min.js"></script>
+
+    <!-- App js -->
+    <script src="assets/js/app.min.js"></script>
+
+</body>
+
+</html>
