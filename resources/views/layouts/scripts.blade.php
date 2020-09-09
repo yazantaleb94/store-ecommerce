7585e1f9292155061dbfb61293069b4759a2a6e2
yazantaleb94 <yazantaleb94@gmail.com>
2020-09-09 09:50:31 +0300

    include and yield adding

---
+++ resources/views/layouts/scripts.blade.php
@@ -0,0 +1,13 @@
+    <!-- Vendor js -->
+    <script src="{{asset('assets/js/vendor.min.js')}}"></script>
+
+    <!--C3 Chart-->
+    <script src="{{asset('assets/libs/d3/d3.min.js')}}"></script>
+    <script src="{{asset('assets/libs/c3/c3.min.js')}}"></script>
+
+    <script src="{{asset('assets/libs/echarts/echarts.min.js')}}"></script>
+
+    <script src="{{asset('assets/js/pages/dashboard.init.js')}}"></script>
+
+    <!-- App js -->
+    <script src="{{asset('assets/js/app.min.js')}}"></script>
